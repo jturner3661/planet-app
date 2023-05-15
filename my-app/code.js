4db8b3d1b4942a0006a69e7a962cbbe6a2594448
jturner3661 <133691367+jturner3661@users.noreply.github.com>
2023-05-15 12:28:24 -0500

    Add files via upload

---
+++ my-app/code.js
@@ -0,0 +1,16 @@
+onEvent("gagiantsbutton", "click", function( ) {
+  setScreen("GasGiants");
+  playSound("assets/category_alerts/airy_bell_notification.mp3", false);
+});
+onEvent("homebutton1", "click", function( ) {
+  setScreen("Homescreen");
+  playSound("assets/category_alerts/airy_bell_notification.mp3", false);
+});
+onEvent("terrestrialbutton", "click", function( ) {
+  setScreen("terrestrial");
+  playSound("assets/category_alerts/airy_bell_notification.mp3", false);
+});
+onEvent("homebutton", "click", function( ) {
+  setScreen("Homescreen");
+  playSound("assets/category_alerts/airy_bell_notification.mp3", false);
+});
